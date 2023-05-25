3354d0af98ed34a37618409cc93dc12280a39083
jbarata <joao.barata@cultofbits.com>
2023-05-25 11:45:55 +0100

    Configurei frase de acknowledge da existência de um README durante o processo de instalaçao do customizet

--- customize.js
+++ customize.js
@@ -1,3 +1,11 @@
 module.exports = {
   version: "1.0.0",
+
+
+  questions: [{
+    type: 'input',
+    name: 'alertReadme',
+    message: 'Please read `others/governance/README.md` file for instructions on how to configure the module (Enter to continue).',
+    default: "ok"
+  }]
 };
