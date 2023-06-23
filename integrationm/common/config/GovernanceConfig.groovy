732741e2ff14e7f7c61e9a63b50f6cf79bd7d5de
jbarata <joao.barata@cultofbits.com>
2023-06-23 13:57:53 +0100

    typos

--- integrationm/common/config/GovernanceConfig.groovy
+++ integrationm/common/config/GovernanceConfig.groovy
@@ -17,9 +17,9 @@ class GovernanceConfig {
      * Custom functions to be used inside Controls' Success Condition code
      * Each entry is the function name and the closure as a string
      * Notes:
-     *  - the closure should be a string so it can be evaluated in the governace script and its 'owner' and 'this' are
+     *  - the closure must be a string so it can be evaluated in the governace script and its 'owner' and 'this' are
      *    correctly set to be the class Script1 that is dinamicaly generated for the success condition of the control
-     *  - The delegate of the closure is set to be the gov_assessment class so we can invoke its gov_assessment methods here
+     *  - The delegate of the closure is set to be the gov_assessment class so we can invoke gov_assessment methods here
      *  - Read https://groovy-lang.org/closures.html for full info on groovy closures
      * Example
      *
